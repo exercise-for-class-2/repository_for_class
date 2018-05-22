1c55fce04b38309d8e846bfc165035f4cdad34f6
takumi13 <38643137+takumi13@users.noreply.github.com>
2018-05-22 17:39:43 +0900

    Add files via upload

--- sekine/make_dat.cpp
+++ sekine/make_dat.cpp
@@ -5,7 +5,6 @@
 #include <vector>
 #include <string>
 #include <stdio.h>
-#include <time.h>
 
 #define GNPLT "C:/PROGRA~1/gnuplot/bin/gnuplot.exe"
 #define X 101   //地図の横の大きさ make_map.cppで計算されたxの値をあらかじめ定義しておく
@@ -21,46 +20,46 @@ bool check(int i,int j,int x,int y,int map[][Y]);
 void input_map(std::string file, int map[][Y], int x, int y);
 //gnuplotで地図を描画するためのfileを作成する
 void output_dat(std::string ofile, int map[][Y], int x, int y);
+//仮想壁を作る
+void change_map(int map[][Y], int map_copy[][Y]);
 
 int main() {
 
-    clock_t start = clock();
     int x = X, y = Y;
     std::string file1 = "map2.dat";
 
     int map[X][Y];
     input_map(file1, map, x, y);          //map[x][y]にfile1の内容を代入していく
+    int map_copy[X][Y];
+    change_map(map, map_copy);
 
     std::string ofile1 = "xy_all2.dat";   //全体を描画するためのfile
     std::string ofile2 = "xy_edges2.dat"; //端点を描画するためのfile
     output_dat(ofile1, map, x, y);        //障害物を記録
     output_dat(ofile2, map, x, y);        //端点を記録
 
-    clock_t end = clock();
-    //演算時間の表示
-    //std::cout << "Time of calculation:" << (double)(end-start)/CLOCKS_PER_SEC << ".sec\n";
     std::cout << "Please check 'xy_all2.dat' & 'xy_edges2.dat'\n";
     std::cout << "Please compile 'Dijkstra_5.cpp'\n";
-    
+
     return 0;
 }
-/*
+
 bool check(int i,int j,int x,int y,int map[][Y]){
-    if(i==0 || i==y-1|| j==0 || j==x-1){
+    if(i==0 || i==x-1|| j==0 || j==y-1){
         return false;
     }
-    if(map[j-1][i]==1 && map[j+1][i]==1){
+    if(map[i-1][j]==1 && map[i+1][j]==1){
         return false;
     }
-    if(map[j][i-1]==1 && map[j][i+1]==1){
+    if(map[i][j-1]==1 && map[i][j+1]==1){
         return false;
     }
-    if(map[j][i]==1){
+    if(map[i][j]==1){
         return true;
     }
     return false;
 }
-*/
+/*
 bool check(int i,int j,int x,int y,int map[][Y]){ 
 
     bool s=false,v=false,ru=false,lu=false,rd=false,ld=false; 
@@ -127,17 +126,17 @@ bool check(int i,int j,int x,int y,int map[][Y]){
      
     return false; 
 } 
-
+*/
 void input_map(std::string file, int map[][Y], int x, int y){
     std::string str;
     int tmp;
     std::ifstream ifile(file.c_str());
-    for(int i=0;i<y;i++){
+    for(int i=0;i<x;i++){
         std::getline(ifile,str);
         std::istringstream iss(str);
         int j=0;
         while(iss >> tmp){
-            map[j][i] = tmp;
+            map[i][j] = tmp;
             j++;
         }
     }
@@ -151,27 +150,35 @@ void output_dat(std::string ofile, int map[][Y], int x, int y){
     if(ofile == "xy_all2.dat"){
         std::ofstream ofp(ofile.c_str());
         ofp << s_x << " " << s_y << '\n';
-        ofp << g_x << " " << g_y << '\n';
-        for(int i=0;i<y;i++){
-            for(int j=0;j<x;j++){
-                if(map[j][i]==1){
-                    ofp << j << " " << i << "\n";
+        for(int i=0;i<x;i++){
+            for(int j=0;j<y;j++){
+                if(map[i][j]==1){
+                    ofp << i << " " << j << "\n";
                 }
             }
         }
+        ofp << g_x << " " << g_y << '\n';
         ofp.close();
     }
     else if(ofile == "xy_edges2.dat"){
         std::ofstream ofp(ofile.c_str());
         ofp << s_x << " " << s_y << '\n';
-        for(int i=0;i<y;i++){
-            for(int j=0;j<x;j++){
+        for(int i=0;i<x;i++){
+            for(int j=0;j<y;j++){
                 if(check(i,j,x,y,map)){
-                    ofp << j << " " << i << "\n";
+                    ofp << i << " " << j << "\n";
                 }
             }
         }
         ofp << g_x << " " << g_y << '\n';
         ofp.close();
+    }
+}
+
+void change_map(int map[][Y], int map_copy[][Y]){
+    for(int i=0; i<X; i++){
+        for(int j=0; j<Y; j++){
+            
+        }
     }
 }
