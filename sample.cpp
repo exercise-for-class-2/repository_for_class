c710e7a4e3d10010f60f72b431fda87d2b63861e
Taiyo2125 <a16.pbb4@g.chuo-u.ac.jp>
2018-05-24 11:04:44 +0900

    地図を読み込んで吐き出すsample.cppの追加

---
+++ sample.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+
+#define X 101   //地図の横の大きさ make_map.cppで計算されたxの値をあらかじめ定義しておく
+#define Y 101   //地図の縦の大きさ make_map.cppで計算されたyの値をあらかじめ定義しておく
+
+void input_map(std::string file, int map[][Y], int x, int y);
+
+int main() {
+
+    int x = X, y = Y;
+    std::string file1 = "map2.dat";
+
+    int map[X][Y];
+    input_map(file1, map, x, y);  
+    for(int i=0;i<X;i++){
+        for(int j=0;j<Y;j++){
+            std::cout << map[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+    return 0;
+}
+
+void input_map(std::string file, int map[][Y], int x, int y){
+    std::string str;
+    int tmp;
+    std::ifstream ifile(file.c_str());
+    for(int i=0;i<x;i++){
+        std::getline(ifile,str);
+        std::istringstream iss(str);
+        int j=0;
+        while(iss >> tmp){
+            map[i][j] = tmp;
+            j++;
+        }
+    }
+}
