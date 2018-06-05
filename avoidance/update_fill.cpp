d64ac8d9d85eeb5536f1e935daf27ffadb7e22e3
sun2125 <taiyo.yuujin@gmail.com>
2018-06-05 17:34:53 +0900

    improve update_fil.cpp

--- avoidance/update_fill.cpp
+++ avoidance/update_fill.cpp
@@ -5,7 +5,7 @@ void update_fil(int **nmap, bool dflag){       //nmap: 障害物情報込みの�
     if(!dflag){
         for(int i=-2;i<3;i++){
             for(int j=-2;j<3;j++){
-                if(nmap[/*今いる階層の値*/][x+i][y+i]!=0){
+                if(nmap[/*今いる階層の値*/][d.x+i][d.y+i]!=0){
                     fil[i+2][j+2] = true;
                 }else{
                     fil[i+2][j+2] = false;
@@ -15,7 +15,7 @@ void update_fil(int **nmap, bool dflag){       //nmap: 障害物情報込みの�
     }else{
         for(int i=0;i<5;i++){
             for(int j=0;j<5j++){
-                if(nmap[x+i-2][y+i-2]!=0){
+                if(nmap[d.x+i-2][d.y+i-2]!=0){
                     fil[4-i][4-j] = true;
                 }else{
                     fil[4-i][4-j] = false;
